840037ae1628113ae7c7361455b888f88ee8416a
admin <mr.seredyn@gmail.com>
2021-12-23 21:13:33 +0200

    Create base posts screen/before integrated FireBase

---
+++ lib/screens/posts_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PostsScreen extends StatefulWidget {
+  const PostsScreen({Key? key}) : super(key: key);
+
+  @override
+  _PostsScreenState createState() => _PostsScreenState();
+}
+
+class _PostsScreenState extends State<PostsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
